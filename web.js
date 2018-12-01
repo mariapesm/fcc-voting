e211857f4766e5c0d1bce32cefe6015688911386
mariapesm <youandmecanyousee@hotmail.com>
2018-12-01 19:43:55 +0200

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 var https = require('https');
 var http = require('http');
 app.get('/', function (req, res) {
- res.sendFile(__dirname + '/public/index.html');
+ res.sendFile(__dirname + '/polls/index.html');
 });
 
 app.listen(process.env.PORT || 3000);
